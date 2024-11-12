e0525b9952de01dcffa0e324bd06685488d640b0
Aenstein <leonid.od@bk.ru>
2024-11-12 14:19:20 +0300

    templates update

--- templating/blogrender.go
+++ templating/blogrender.go
@@ -4,11 +4,19 @@ import (
 	"embed"
 	"html/template"
 	"io"
+	"strings"
+
+	"github.com/gomarkdown/markdown"
+	"github.com/gomarkdown/markdown/parser"
 )
 
 type Post struct {
 	Title, Body, Description string
 	Tags					 []string
+}
+
+func (p Post) SanitisedTitle() string {
+	return strings.ToLower(strings.Replace(p.Title, " ", "-", -1))
 }
 
 var (
@@ -18,6 +26,8 @@ var (
 
 type PostRenderer struct {
 	templ *template.Template
+	mdParser *parser.Parser
+
 }
 
 func NewPostRenderer() (*PostRenderer, error) {
@@ -26,14 +36,27 @@ func NewPostRenderer() (*PostRenderer, error) {
 		return nil, err
 	}
 
-	return &PostRenderer{templ: templ}, nil
+	extensions := parser.CommonExtensions | parser.AutoHeadingIDs
+	parser := parser.NewWithExtensions(extensions)
+
+	return &PostRenderer{templ: templ, mdParser: parser}, nil
 }
 
 func (r *PostRenderer) Render(w io.Writer, p Post) error {
+	return r.templ.ExecuteTemplate(w, "blog.gohtml", newPostVM(p, r))
+}
 
-	if err := r.templ.ExecuteTemplate(w, "blog.gohtml", p); err != nil {
-		return err
-	}
+func (r *PostRenderer) RenderIndex(w io.Writer, posts []Post) error {
+	return r.templ.ExecuteTemplate(w, "index.gohtml", posts)
+}
 
-	return nil
+type postViewModel struct {
+	Post
+	HTMLBody template.HTML
+}
+
+func newPostVM(p Post, r *PostRenderer) postViewModel {
+	vm := postViewModel{Post: p}
+	vm.HTMLBody = template.HTML(markdown.ToHTML([]byte(p.Body), r.mdParser, nil))
+	return vm
 }
